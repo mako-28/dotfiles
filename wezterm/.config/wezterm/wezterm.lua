4d7edfe1a572f4607d3098b1794a11bff792730f
Makoto TSUCHIDA <e28748@jp.ibm.com>
2023-11-03 21:38:44 +0900

    initial.
    as of Nov. 2023

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,42 @@
+local wezterm = require 'wezterm';
+local mux = wezterm.mux
+
+return {
+  font = wezterm.font_with_fallback({
+    "UDEV Gothic 35NF",
+  }),
+  font_size = 14.5,
+  line_height = 1.2,
+  --  color_scheme = "OneHalfDark", -- 自分の好きなテーマ探す https://wezfurlong.org/wezterm/colorschemes/index.html
+  --  color_scheme = "Japanesque",
+  color_scheme = "Ayu Mirage",
+  hide_tab_bar_if_only_one_tab = true,
+  adjust_window_size_when_changing_font_size = false,
+  macos_forward_to_ime_modifier_mask = "SHIFT|CTRL",
+
+
+  wezterm.on("gui-startup", function()
+    local tab, pane, window = mux.spawn_window{}
+    window:gui_window():maximize()
+  end),
+
+  -- timeout_milliseconds defaults to 1000 and can be omitted
+  leader = { key = 'a', mods = 'CMD', timeout_milliseconds = 1000 },
+  keys = {
+    {
+      key = '|',
+      mods = 'LEADER|SHIFT',
+      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = '=',
+      mods = 'LEADER',
+      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = 'g',
+      mods = 'LEADER',
+      action = wezterm.action.PaneSelect,
+    },
+  },
+}
