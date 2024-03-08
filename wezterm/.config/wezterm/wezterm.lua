7375bbdb5a72b49db496dd0130321a4aa19dc632
Makoto TSUCHIDA <e28748@jp.ibm.com>
2024-03-08 15:46:03 +0900

    wezterm: scrollbackを増やす
    3500->10000

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -13,6 +13,7 @@ return {
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
   macos_forward_to_ime_modifier_mask = "SHIFT|CTRL",
+  scrollback_lines = 10000,
 
 
   wezterm.on("gui-startup", function()
