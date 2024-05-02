d2922da41aed1b174b6bd7b897e3c5038faa9e61
Makoto TSUCHIDA <e28748@jp.ibm.com>
2024-05-02 15:13:21 +0900

    wezterm: リンククリックでURLを開かない
    Cmd+クリックで開くように変更

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -90,5 +90,25 @@ return {
       action = wezterm.action.AdjustPaneSize { 'Right', 30 },
     },
   },
+  mouse_bindings = {
+    -- Disable the default click behavior
+    {
+      event = { Up = { streak = 1, button = "Left"} },
+      mods = "NONE",
+      action = wezterm.action.DisableDefaultAssignment,
+    },
+    -- Ctrl-click will open the link under the mouse cursor
+    {
+        event = { Up = { streak = 1, button = "Left" } },
+        mods = "CMD",
+        action = wezterm.action.OpenLinkAtMouseCursor,
+    },
+    -- Disable the Ctrl-click down event to stop programs from seeing it when a URL is clicked
+    {
+        event = { Down = { streak = 1, button = "Left" } },
+        mods = "CMD",
+        action = wezterm.action.Nop,
+    },
+  },
 }
 
