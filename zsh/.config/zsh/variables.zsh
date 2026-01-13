fb08e6a348f548582bef68c3a6d86fc39a887754
Makoto TSUCHIDA <tsuchida.makoto@gmail.com>
2026-01-13 15:27:23 +0900

    zsh: javaの存在判定を削除

--- zsh/.config/zsh/variables.zsh
+++ zsh/.config/zsh/variables.zsh
@@ -5,14 +5,5 @@ export SUDO_EDITOR=vi
 # for docker-compose
 export UID
 
-# Define default_java for cross-platform JAVA_HOME resolution
-if [[ -z "${DEFAULT_JAVA}" ]]; then # Only set if not already defined
-  if [[ "$(uname)" == "Darwin" ]]; then
-    DEFAULT_JAVA=$(/usr/libexec/java_home)
-  else
-    DEFAULT_JAVA=$(dirname $(dirname $(readlink -f $(which java))))
-  fi
-fi
-
 # for less
 export LESSHISTFILE="${XDG_STATE_HOME}/less/history"
